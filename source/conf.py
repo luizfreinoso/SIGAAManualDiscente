22057a4594ede5db4f9c2a68093f37b0cd435132
LUIZ FERNANDO REINOSO <luiz.reinoso@ifpa.local>
2020-01-24 15:50:09 -0300

    Testes de tradução no Read the Docs

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'pt_BR'
+language = 'pt'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
